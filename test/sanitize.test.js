9562b5006304b65c658f03db5a6dc146527ba09d
Stewart Lynch <stewart.j.lynch@gmail.com>
2018-04-19 11:49:40 -0700

    feat(nested-overrides): add option

--- test/sanitize.test.js
+++ test/sanitize.test.js
@@ -314,4 +314,129 @@ describe('sanitize', () => {
     });
   });
 
+  describe('nestedOverrides', () => {
+
+    it('uses options for nested fields', () => {
+      const input = {
+        key: null
+      };
+
+      const nestedInput = {
+        key: {
+          nested: null
+        }
+      };
+
+      const options = {
+        stripNull: true,
+        nestedOverrides: {
+          key: {
+            stripNull: false
+          }
+        }
+      };
+
+      const result = Sanitize(input, options);
+      const nestedResult = Sanitize(nestedInput, options);
+
+      expect(result).to.eql({});
+      expect(nestedResult).to.eql(nestedInput);
+    });
+
+    it('overrides fieldOverrides options', () => {
+      const input = {
+        key: {
+          nested: null
+        }
+      };
+
+      const options = {
+        pruneMethod: 'replace',
+        replaceValue: 0,
+        stripNull: true,
+        fieldOverrides: {
+          key: {
+            replaceValue: 2
+          }
+        },
+        nestedOverrides: {
+          key: {
+            replaceValue: 1
+          }
+        }
+      };
+
+      const result = Sanitize(input, options);
+
+      expect(result).to.eql({
+        key: {
+          nested: 1
+        }
+      });
+    });
+
+    it('ignores nestedOverrides if not object', () => {
+      const input = {
+        key: null
+      };
+
+      const options = {
+        pruneMethod: 'replace',
+        replaceValue: 0,
+        stripNull: true,
+        fieldOverrides: {
+          key: {
+            replaceValue: 2
+          }
+        },
+        nestedOverrides: {
+          key: {
+            replaceValue: 1
+          }
+        }
+      };
+
+      const result = Sanitize(input, options);
+
+      expect(result).to.eql({ key: 2 });
+    });
+
+    it('do not affect other nested objects', () => {
+      const input = {
+        key: {
+          key2: {
+            key: null
+          }
+        },
+        other: null
+      };
+
+      const options = {
+        pruneMethod: 'replace',
+        replaceValue: 0,
+        stripNull: true,
+        nestedOverrides: {
+          key: {
+            replaceValue: 1
+          },
+          key2: {
+            replaceValue: 2
+          }
+        }
+      };
+
+      const result = Sanitize(input, options);
+
+      expect(result).to.eql({
+        key: {
+          key2: {
+            key: 1
+          }
+        },
+        other: 0
+      });
+    });
+
+  });
+
 });
